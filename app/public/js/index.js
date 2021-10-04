b8881d28bc13b8398b7f368ebaff9f8ca278abf5
Satvika Kalia <satkalia@iu.edu>
2021-10-04 14:16:31 -0400

    Update
    Used Tom's GitHub repository to replicate in-class practice material

---
+++ app/public/js/index.js
@@ -0,0 +1,37 @@
+
+const SomeApp = {
+    data() {
+      return {
+        result: undefined,
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.result.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            //Method 1:
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.result = json.results[0];
+                console.log("C");
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchUserData();
+    }
+
+  }
+  
+  Vue.createApp(SomeApp).mount('#someApp');
